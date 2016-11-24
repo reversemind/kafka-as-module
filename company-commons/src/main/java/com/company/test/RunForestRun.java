d4cc24c9c7bf04d1d01889fa565bbadf0cf3ec3b
Eugene Kalinin <kalinin.eugene@gmail.com>
2016-11-24 11:32:49 +0300

    Initial commit

---
+++ company-commons/src/main/java/com/company/test/RunForestRun.java
@@ -0,0 +1,13 @@
+package com.company.test;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+/**
+ *
+ */
+public class RunForestRun {
+
+    public static void main(String... args) {
+        KafkaConsumer<String, String> consumer;
+    }
+}
